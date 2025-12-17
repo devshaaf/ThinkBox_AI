8bec89bbb823de380adf8355e68029629b392691
devshaaf <mrj71902@gmail.com>
2025-12-17 09:53:05 +0500

    modified user model

--- users/models.py
+++ users/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to='avatars/', default="avatars/user.png", blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', default="avatars/user.png", blank=True)
